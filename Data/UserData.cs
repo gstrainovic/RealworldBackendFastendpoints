e23b89b9a14af7ca16a2e6bc397dc9aa5322b83b
gstrainovic <g.strainovic@gmail.com>
2023-02-23 23:16:15 +0100

    upd

--- Data/UserData.cs
+++ Data/UserData.cs
@@ -1,6 +1,17 @@
+using System.Security.Claims;
+using AgileObjects.AgileMapper.Extensions;
+
+public enum FollowHandler
+{
+  add,
+  remove,
+  ignore
+}
 
 public class UserData
 {
+
+
   public static async Task<Ent.User> GetUserByEmail(string email)
   {
     return await DB.Find<Ent.User>()
@@ -23,4 +34,33 @@ public class UserData
       .ExecuteAsync();
   }
 
+  public static async Task<ProfileResponse> Follow(string UserNameRequest, string CurrentUserEmail, FollowHandler follow)
+  {
+    Ent.User UserFromRequest = await GetUserByUserName(UserNameRequest);
+    ProfileResponse.profile Response = UserFromRequest.Map().ToANew<ProfileResponse.profile>();
+
+    if (CurrentUserEmail != null)
+    {
+          Ent.User currentUser = await GetUserByEmail(CurrentUserEmail);
+          if (follow == FollowHandler.add)
+          {
+            currentUser.Following.Add(UserFromRequest.Email);
+            await currentUser.SaveAsync();
+          }
+          else if (follow == FollowHandler.remove)
+          {
+            currentUser.Following.Remove(UserFromRequest.Email);
+            await currentUser.SaveAsync();
+          }
+          Response.following = currentUser.Following.Contains(UserFromRequest.Email);
+    }
+
+    return new()
+    {
+      Profile = Response
+    };
+  }
+
 }
+
+// enum follow
--- Endpoints/FollowProfile.cs
+++ Endpoints/FollowProfile.cs
@@ -3,18 +3,17 @@ using System.Text.Json;
 using AgileObjects.AgileMapper.Extensions;
 using FastEndpoints;
 
-public class GetProfileEndpoint : EndpointWithoutRequest<ProfileResponse>
+public class FollowProfileEndpoint : EndpointWithoutRequest<ProfileResponse>
 {
     public override void Configure()
     {
-        Get("api/profiles/{username}");
-        AllowAnonymous();
+        Post("api/profiles/{username}/follow");
     }
 
     public override async Task HandleAsync(CancellationToken ct)
     {
         string UserNameRequest = Route<string>("username");
         string CurrentUserEmail = User.FindFirstValue(ClaimName.UserEmail);
-        await SendAsync(await UserData.Follow(UserNameRequest, CurrentUserEmail, FollowHandler.ignore));
-    }
+        await SendAsync(await UserData.Follow(UserNameRequest, CurrentUserEmail, FollowHandler.add));
+  }
 }
