9bf3842fa8369043e054b79b1bf8cbbeb10b3a0e
gstrainovic <g.strainovic@gmail.com>
2023-02-25 21:10:29 +0100

    upd

---
+++ Entity/Profile.cs
@@ -0,0 +1,16 @@
+namespace Ent;
+
+public class Article : Entity 
+{
+  public string? Slug { get; set; }
+  public string? Title { get; set; }
+  public string? Description { get; set; }
+  public string? Body { get; set; }
+  public HashSet<string> TagList { get; set; } = new();
+  public DateTime CreatedAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
+  public bool Favorited { get; set; }
+  public int FavoritesCount { get; set; }
+  public string? AuthorId { get; set; }
+  public One<Ent.User>? Author { get; set; }
+}
---
+++ Models/Response/Article.cs
@@ -0,0 +1,50 @@
+﻿namespace Models.Response;
+
+public class ArticleResponse
+{
+// {
+//   "article": {
+//     "slug": "how-to-train-your-dragon",
+//     "title": "How to train your dragon",
+//     "description": "Ever wonder how?",
+//     "body": "It takes a Jacobian",
+//     "tagList": ["dragons", "training"],
+//     "createdAt": "2016-02-18T03:22:56.637Z",
+//     "updatedAt": "2016-02-18T03:48:35.824Z",
+//     "favorited": false,
+//     "favoritesCount": 0,
+//     "author": {
+//       "username": "jake",
+//       "bio": "I work at statefarm",
+//       "image": "https://i.stack.imgur.com/xHWG8.jpg",
+//       "following": false
+//     }
+//   }
+// }
+
+  public Ent.Article? Article { get; set; }
+  // public class article
+  // {
+  //   public string? slug { get; set; }
+  //   public string? title { get; set; }
+  //   public string? description { get; set; }
+  //   public string? body { get; set; }
+  //   public string? tagList { get; set; }
+  //   public string? createdAt { get; set; }
+  //   public string? updatedAt { get; set; }
+  //   public bool favorited { get; set; }
+  //   public int favoritesCount { get; set; }
+  //   public author? Author { get; set; }
+  //   public class author
+  //   {
+  //     public string? username { get; set; }
+  //     public string? bio { get; set; }
+  //     public string? image { get; set; }
+  //     public bool following { get; set; }
+  //   }
+  // }
+
+}
+
+
+
